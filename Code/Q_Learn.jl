c9b79945f7fcd43295a592157298ffbc06303445
mwdacus <mwdacus@gmail.com>
2021-11-22 18:43:44 -0800

    add mutable struct

--- Code/Q_Learn.jl
+++ Code/Q_Learn.jl
@@ -1,2 +1,22 @@
-#This is a test
-println("Hello World")
+#Michael Dacus                                    AA 228-Final Project                        Team 34
+# Ian Hokai
+# Tyler Weiss
+#
+#Objective: Based on previous data, use Q-Learning to determine approximate optimal policy for each state
+#Input: Incoming Dataset (s,a,r,sp)
+#Output: Best Action at each state
+#----------------------------------------------------------------------------------
+
+include('main.jl')
+
+#Define Q-Learning Variable
+mutable struct QLearning
+    𝒮
+    𝒜
+    γ
+    Q
+    α
+end
+
+model=MDP(S)
+#Update Action Value function
