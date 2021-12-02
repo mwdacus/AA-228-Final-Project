01fcde4948302f6a64458cc9800d3ae1054b5a15
mwdacus <mwdacus@gmail.com>
2021-12-01 21:43:52 -0800

    began running simulations

--- Code/SimulateScenario2.jl
+++ Code/SimulateScenario2.jl
@@ -21,43 +21,41 @@ using Distributions
 using DataFrames
 using CSV
 
+#Call Scenario 2
 include("MDPScenario2.jl")
+
 #Determine the probability the team will score a touchdown (for each team)
-p_td=0.5;
+p_td=0.3;
+
+#probability of succesfully making extra point
 p_kick=mean.(params.p_suc_kick)*(1-mean.(params.p_stop_kick));
 p_two=mean.(params.p_suc_two)*(1-mean.(params.p_stop_two));
 
-#Call Scenario 2
-
-#create an empty DataFrame Table (sars′)
-sars′=DataFrame(s=State[],a=Action[],r=Int[],s′=State[])
-
-#Simulate Game
+#Simulate Game function
 function simulate(sarsp,mdp)
     #Assume 12 offensive drives in a game
     for i in 1:12
         #offensive possession
         if rand(Bernoulli(p_td))==true
             global params
-            local solver
             local policy
-            if sarsp.s==[]
+            if sarsp.s′==[] || sarsp.s′[end]==params.termination_state
                 s1=State(6)
             else
                 s1=State(6+sarsp.s′[end].PS)
             end
-            solver=ValueIterationSolver(max_iterations=100)
-            policy=solve(solver,mdp)
+            #call function solver type
+            policy=MDPSolvingMethod(mdp)
             a1=action(policy,s1)
             if a1==kick && rand(Bernoulli(p_kick))==true
                 s1′=State(s1.PS+1)
-                r1=R(s1,a1,s1′)
+                r1=round(R(s1,a1),digits=3)
             elseif a1==two && rand(Bernoulli(p_two))==true
                 s1′=State(s1.PS+2)
-                r1=R(s1,a1,s1′)
+                r1=round(R(s1,a1),digits=3)
             else
                 s1′=s1
-                r1=0
+                r1=round(R(s1,a1),digits=3)
             end        
             push!(sarsp,[s1,a1,r1,s1′])
             #update(sarsp,params)
@@ -65,26 +63,50 @@ function simulate(sarsp,mdp)
         else
             #if opponent scores, assume they go for td, if they get it
             if rand(Bernoulli(p_td))==true
-                if sarsp.s′==[]
+                if sarsp.s′==[] || sarsp.s′[end]==params.termination_state
                     opp=State(-7)
                 else
                     opp=State(sarsp.s′[end].PS-7)
                 end
             #if they don't
             else
-                if sarsp.s′==[]
+                if sarsp.s′==[] || sarsp.s′[end]==params.termination_state
                     opp=State(0)
                 else
                     opp=State(sarsp.s′[end].PS)
                 end
             end
-            push!(sarsp,[State(0),kick,1,opp])
+            push!(sarsp,[params.termination_state,kick,1,opp])
         end
     end
+    # Determine if they win or lose, and add to sars′
+    if sarsp.s′[end].PS>0
+        s_end=params.win_state
+        a_end=kick
+        r_end=r1=round(R(s_end,a_end),digits=3)
+        s′_end=params.termination_state
+    else
+        s_end=params.lose_state
+        a_end=kick
+        r_end=r1=round(R(s_end,a_end),digits=3)
+        s′_end=params.termination_state
+    end
+    push!(sarsp,[s_end,a_end,r_end,s′_end])
     return sarsp
 end
 
-#=
+#function for determining method of solving
+function MDPSolvingMethod(mdp)
+    if mdp==q_mdp
+        q_learning_solver = QLearningSolver(n_episodes=100,learning_rate=0.3,exploration_policy=EpsGreedyPolicy(mdp, 0.5),verbose=false);
+        return solve(q_learning_solver, mdp);
+    else
+        solver=ValueIterationSolver(max_iterations=100)
+            return solve(solver,mdp)
+    end
+end
+
+#=function for updating parameters
 function update(sarsp,params)
 	if sarsp.s′[end]==kickmade
 		params.p_suc_kick=Beta(params.p_suc_kick.α+1,params.p_suc_kick.β)
@@ -102,9 +124,26 @@ function update(sarsp,params)
 end
 =#
 
+#function for determining average win percentage
+function WinPercent(sarsp)
+    (row,col)=size(sarsp)
+    num_wins=zeros(row)
+    num_losses=zeros(row)
+    win_percentage=zeros(row)
+    for i in 50:50:row
+        num_wins[i]=count(x->(x==params.win_state),sarsp.s[1:i])
+        num_losses[i]=count(x->(x==params.lose_state),sarsp.s[1:i])
+        win_percentage[i]=num_wins[i]/num_losses[i]
+    end
+    return win_percentage
+end
 
-simulate(sars′,mdp)
-sars′
-
-
-CSV.write("test.csv",sars′)
+#main Script Line
+#create an empty DataFrame Table (sars′)
+sars′=DataFrame(s=State[],a=Action[],r=Float64[],s′=State[]);
+#state number of iterations
+k=100;
+for j in 1:100   
+    simulate(sars′,mdp)
+end
+CSV.write("simulation.csv",sars′)
